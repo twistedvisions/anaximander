363be59b2a2ccae3a689f1b18cf1d70eb35e20ff
William Becker <wbecker+hiveshare@gmail.com>
2013-08-18 20:31:53 +0100

    add nodejs server

--- db/get_event_attendees_at_point.sql
+++ db/get_event_attendees_at_point.sql
@@ -5,6 +5,6 @@ inner join event_attendee on event_attendee.event_id = event.id
 inner join person on person.id = event_attendee.person_id
 where ST_CONTAINS(
   ST_BuildArea(place.location),
-  ST_PointFromText('POINT(147.325 -42.880556  )')
+  ST_PointFromText('POINT(147.325 -42.880556)')
 )  = TRUE
 ;
---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8000);
+
+console.log('Listening on port 8000');
