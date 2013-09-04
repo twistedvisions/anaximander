77fdb2134660d36fb40d4757a124e9a42142be13
William Becker <wbecker+hiveshare@gmail.com>
2013-09-04 08:14:32 +0100

    only show closest 200 points

--- db_templates/get_event_attendees_at_point.sql
+++ db_templates/get_event_attendees_at_point.sql
@@ -5,14 +5,20 @@ select
   event.link as event_link, 
   event.start_date,
   event.end_date,
-  ST_AsText(place.location) as location
+  ST_AsText(place.location) as location,
+  ST_MaxDistance (
+    place.location,
+    ST_PointFromText('POINT(<%= lon %> <%= lat %>)')
+  ) as distance
 from place 
 inner join event on event.place_id = place.id
 left join event_attendee on event_attendee.event_id = event.id
 left join person on event_attendee.person_id = person.id
-where ST_MaxDistance (
+where  ST_MaxDistance (
   place.location,
   ST_PointFromText('POINT(<%= lon %> <%= lat %>)')
 ) < <%= radius %>
 and event.start_date >= '<%= start %>'
 and event.end_date <= '<%= end %>'
+order by distance asc
+limit 200
