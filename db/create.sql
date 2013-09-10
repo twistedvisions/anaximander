4cc9e5847332969b9c9a05d72d8ed01d323893e5
William Becker <wbecker+hiveshare@gmail.com>
2013-09-10 22:52:58 +0100

    Add construction of buildings to events

--- db/create.sql
+++ db/create.sql
@@ -46,3 +46,4 @@ CREATE TABLE event_participant
 
 INSERT INTO thing_type (name) VALUES ('person');
 INSERT INTO thing_type (name) VALUES ('organisation');
+INSERT INTO thing_type (name) VALUES ('construction');
