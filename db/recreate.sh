c00b025cb8f86ca66a9e92d4b1a385e4d12e1234
William Becker <wbecker+hiveshare@gmail.com>
2013-09-11 08:30:34 +0100

    Load the file in chunks

--- db/recreate.sh
+++ db/recreate.sh
@@ -1,1 +1,9 @@
-time redis-cli FLUSHALL && time ./db/create.sh && time node lib/parser/parser.js 
+time redis-cli FLUSHALL && time ./db/create.sh && 
+  time node lib/parser/parser.js --start 0 --finish 25 -p  && 
+  time node lib/parser/parser.js --start 25 --finish 50 -p && 
+  time node lib/parser/parser.js --start 50 --finish 75 -p && 
+  time node lib/parser/parser.js --start 75 --finish 100 -p && 
+  time node lib/parser/parser.js --start 0 --finish 25 -e  && 
+  time node lib/parser/parser.js --start 25 --finish 50 -e && 
+  time node lib/parser/parser.js --start 50 --finish 75 -e && 
+  time node lib/parser/parser.js --start 75 --finish 100 -e
