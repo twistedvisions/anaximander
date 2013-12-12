027a49458271c30a4a0a4c0a07c7b38be6ff6cdb
William Becker <wbecker+hiveshare@gmail.com>
2013-12-12 15:24:53 +0000

    Refactor out login-local

---
+++ public/js/views/login_local.js
@@ -0,0 +1,100 @@
+define([
+  "jquery",
+  "underscore",
+  "backbone",
+  "sha256",
+  "when",
+  "text!templates/login-retred.htm",
+  "bootstrap",
+  "css!/css/login"
+], function ($, _, Backbone, sha256, when, template) {
+
+  var Login = Backbone.View.extend({
+ 
+    initialize: function (options) {
+      this.target = options.target;
+    },
+
+    render: function () {
+      this.$(this.target).popover({
+        placement: "top",
+        html: true,
+        animation: false,
+        content: template
+      });
+      this.$(this.target).on("shown.bs.popover", _.bind(this.handleLoginShown, this));
+
+      return this.$el;
+    },
+
+    handleLoginShown: function () {
+      this.$(".popover .register").on("click", _.bind(this.handleRegister, this));
+      this.$(".popover .login").on("click", _.bind(this.handleLogin, this));
+      this.$(".popover .dismiss").on("click", _.bind(this.handleClose, this));
+      this.$(".popover form input[name=username]").focus();
+      this.$(".popover form").on("submit", _.bind(this.handleLogin, this));
+    },
+
+    handleRegister: function () {
+      when($.post("/register", {
+        username: this.$(".popover form input[name=username]").val(),
+        password: sha256.SHA256(this.$(".popover form input[name=password]").val()).toString()
+      })).then(
+        _.bind(function () {
+          window.console.log("done", arguments);
+          this.$("#login-retred").popover("toggle");
+        }, this), 
+        this.handleFailedRequest("Registration failed")
+      );
+    },
+
+    handleLogin: function (e) {
+      when($.post("/login", {
+        username: this.$(".popover form input[name=username]").val(),
+        password: sha256.SHA256(this.$(".popover form input[name=password]").val()).toString()
+      })).then(
+        _.bind(function () {
+          window.console.log("done", arguments);
+          this.$("#login-retred").popover("toggle");
+        }, this),
+        this.handleFailedRequest("Login failed")
+      );
+      e.preventDefault();
+    },
+
+    handleFailedRequest: function (defaultErrorMessage) {
+      return _.bind(function (err) {
+
+        var movePopup = false;
+        if (!this.$(".popover form .alert").is(":visible")) {
+          movePopup = true;
+        }
+
+        var oldMargin = 
+          parseInt(this.$(".popover form .alert").css("margin-top"), 10) + 
+          parseInt(this.$(".popover form .alert").css("margin-bottom"), 10);
+
+        this.$(".popover form .alert").text(err.responseText || defaultErrorMessage);
+        this.$(".popover form .alert").show();
+
+        if (movePopup) {
+          this.$(".popover").css("top", 
+            parseInt(this.$(".popover").css("top"), 10) - 
+            (
+              parseInt(this.$(".popover form .alert").css("height"), 10) +
+              oldMargin
+            )
+          );
+        }
+      }, this);
+    },
+
+    handleClose: function () {
+      this.$("#login-retred").popover("toggle");
+    }
+
+  });
+
+  return Login;
+
+});
