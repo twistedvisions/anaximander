0d45dd44e701e8b66abbe7fac8e38fe7149d1582
William Becker <wbecker+hiveshare@gmail.com>
2013-12-22 22:04:36 +0000

    test login local

--- public/js/views/login_local.js
+++ public/js/views/login_local.js
@@ -2,28 +2,30 @@ define([
   "jquery",
   "underscore",
   "backbone",
+  "bootstrap",
   "sha256",
   "when",
   "text!templates/login-retred.htm",
   "bootstrap",
   "css!/css/login"
-], function ($, _, Backbone, sha256, when, template) {
+], function ($, _, Backbone, bootstrap, sha256, when, template) {
 
   var Login = Backbone.View.extend({
- 
+    registrationFailedMessage: "Registration failed",
+    loginFailedMessage: "Login failed",
+
     initialize: function (options) {
-      this.target = options.target;
       this.user = options.user;
     },
 
     render: function () {
-      this.$(this.target).popover({
+      this.$("#login-retred").popover({
         placement: "top",
         html: true,
         animation: false,
         content: template
       });
-      this.$(this.target).on("shown.bs.popover", _.bind(this.handleLoginShown, this));
+      this.$("#login-retred").on("shown.bs.popover", _.bind(this.handleLoginShown, this));
 
       return this.$el;
     },
@@ -41,12 +43,18 @@ define([
         username: this.$(".popover form input[name=username]").val(),
         password: sha256.SHA256(this.$(".popover form input[name=password]").val()).toString()
       })).then(
-        _.bind(function () {
-          this.$("#login-retred").popover("toggle");
-          this.user.set("logged-in", true);
-        }, this), 
-        this.handleFailedRequest("Registration failed")
+        _.bind(this.handleRegisterSuccess, this), 
+        _.bind(this.handleRegisterFailure, this)
       );
+    },
+
+    handleRegisterSuccess: function () {
+      this.$("#login-retred").popover("toggle");
+      this.user.set("logged-in", true);
+    },
+
+    handleRegisterFailure: function (err) {
+      this.handleFailedRequest(this.registrationFailedMessage, err);
     },
 
     handleLogin: function (e) {
@@ -54,40 +62,43 @@ define([
         username: this.$(".popover form input[name=username]").val(),
         password: sha256.SHA256(this.$(".popover form input[name=password]").val()).toString()
       })).then(
-        _.bind(function () {
-          this.$("#login-retred").popover("toggle");
-          this.user.set("logged-in", true);
-        }, this),
-        this.handleFailedRequest("Login failed")
+        _.bind(this.handleLoginSuccess, this),
+        _.bind(this.handleLoginFailure, this)
       );
       e.preventDefault();
     },
 
-    handleFailedRequest: function (defaultErrorMessage) {
-      return _.bind(function (err) {
+    handleLoginSuccess: function () {
+      this.$("#login-retred").popover("toggle");
+      this.user.set("logged-in", true);
+    },
 
-        var movePopup = false;
-        if (!this.$(".popover form .alert").is(":visible")) {
-          movePopup = true;
-        }
+    handleLoginFailure: function (err) {
+      this.handleFailedRequest(this.loginFailedMessage, err);
+    },
 
-        var oldMargin = 
-          parseInt(this.$(".popover form .alert").css("margin-top"), 10) + 
-          parseInt(this.$(".popover form .alert").css("margin-bottom"), 10);
+    handleFailedRequest: function (defaultErrorMessage, err) {
+      var movePopup = false;
+      if (!this.$(".popover form .alert").is(":visible")) {
+        movePopup = true;
+      }
 
-        this.$(".popover form .alert").text(err.responseText || defaultErrorMessage);
-        this.$(".popover form .alert").show();
+      var oldMargin = 
+        parseInt(this.$(".popover form .alert").css("margin-top"), 10) + 
+        parseInt(this.$(".popover form .alert").css("margin-bottom"), 10);
 
-        if (movePopup) {
-          this.$(".popover").css("top", 
-            parseInt(this.$(".popover").css("top"), 10) - 
-            (
-              parseInt(this.$(".popover form .alert").css("height"), 10) +
-              oldMargin
-            )
-          );
-        }
-      }, this);
+      this.$(".popover form .alert").text(err.responseText || defaultErrorMessage);
+      this.$(".popover form .alert").show();
+
+      if (movePopup) {
+        this.$(".popover").css("top", 
+          parseInt(this.$(".popover").css("top"), 10) - 
+          (
+            parseInt(this.$(".popover form .alert").css("height"), 10) +
+            oldMargin
+          )
+        );
+      }
     },
 
     handleClose: function () {
--- test/webapp/views/login-local.js
+++ test/webapp/views/login-local.js
@@ -1,25 +1,103 @@
-/*global describe, it */
+/*global describe, it, beforeEach, afterEach */
 define(
 
-  ["backbone", "views/login_local"], 
+  ["chai", "jquery", "backbone", "views/login_local"], 
 
-  function (/*Backbone, LoginLocal*/) {
-
+  function (chai, $, Backbone, LoginLocal) {
+    
+    var should = chai.should();
+    
     describe("interaction", function () {
 
-      it("should attach a popover the the login local button");
+      beforeEach(function () {
+        this.targetEl = $("<div id='some-container'><div id='login-retred'></div></div>");
+        this.targetEl.appendTo(document.body);
 
-      it("should hide the popup after registration success");
-      it("should log the user in after registration success");
-      it("should show a message upon registration failure");
+        this.loginLocal = new LoginLocal({
+          el: "#some-container",
+          user: new Backbone.Model({
+            "logged-in": false
+          })
+        });
+      });
 
-      it("should hide the popup after login success");
-      it("should log the user in after login success");
-      it("should show a message upon login failure");
+      afterEach(function () {
+        this.targetEl.remove();
+      });
 
-      it("should allow a user to cancel logging in by pressing the close button");
-      it("should allow a user to cancel logging in by pressing escape");
+      it("should attach a popover the the login local button", function () {
+        should.not.exist(this.loginLocal.$("#login-retred").data()["bs.popover"]);
+        this.loginLocal.render();
+        should.exist(this.loginLocal.$("#login-retred").data()["bs.popover"]);
+      });
 
+      describe("registration", function () {        
+
+        it("should hide the popup after registration success", function () { 
+          this.loginLocal.render();
+          this.loginLocal.$("#login-retred").popover("show");
+          this.loginLocal.$("#login-retred").data()["bs.popover"]
+            .tip().hasClass("in").should.equal(true);
+          this.loginLocal.handleRegisterSuccess();
+          this.loginLocal.$("#login-retred").data()["bs.popover"]
+            .tip().hasClass("in").should.equal(false);
+        });
+
+        it("should log the user in after registration success", function () {
+          this.loginLocal.render();
+          this.loginLocal.handleRegisterSuccess();
+          this.loginLocal.user.get("logged-in").should.equal(true);
+        });
+
+        it("should show a message upon registration failure", function () {
+          this.loginLocal.render();
+          this.loginLocal.$("#login-retred").popover("show");
+          this.loginLocal.$(".popover form .alert").text().should.equal("");
+          this.loginLocal.handleRegisterFailure({});
+          this.loginLocal.$(".popover form .alert").text()
+            .should.equal(this.loginLocal.registrationFailedMessage);
+        });
+
+      });
+
+      describe("logging in", function () {
+
+        it("should hide the popup after login success", function () {
+          this.loginLocal.render();
+          this.loginLocal.$("#login-retred").popover("show");
+          this.loginLocal.$("#login-retred").data()["bs.popover"]
+            .tip().hasClass("in").should.equal(true);
+          this.loginLocal.handleLoginSuccess();
+          this.loginLocal.$("#login-retred").data()["bs.popover"]
+            .tip().hasClass("in").should.equal(false);
+        });
+
+        it("should log the user in after login success", function () {
+          this.loginLocal.render();
+          this.loginLocal.handleLoginSuccess();
+          this.loginLocal.user.get("logged-in").should.equal(true);
+        });
+
+        it("should show a message upon login failure", function () {
+          this.loginLocal.render();
+          this.loginLocal.$("#login-retred").popover("show");
+          this.loginLocal.$(".popover form .alert").text().should.equal("");
+          this.loginLocal.handleLoginFailure({});
+          this.loginLocal.$(".popover form .alert").text()
+            .should.equal(this.loginLocal.loginFailedMessage);
+        });
+        
+      });
+
+      it("should allow a user to cancel logging in by pressing the close button", function () {
+        this.loginLocal.render();
+        this.loginLocal.$("#login-retred").popover("show");
+        this.loginLocal.$("#login-retred").data()["bs.popover"]
+          .tip().hasClass("in").should.equal(true);
+        this.loginLocal.handleClose();
+        this.loginLocal.$("#login-retred").data()["bs.popover"]
+          .tip().hasClass("in").should.equal(false);
+      });
     });
 
   }
