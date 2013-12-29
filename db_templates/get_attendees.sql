4a56f9e2d52eadcb6e464f961c4d0bd186706caa
William Becker <wbecker+hiveshare@gmail.com>
2013-12-29 19:21:51 +0000

    check for things in the middle of the query, not just the start

--- db_templates/get_attendees.sql
+++ db_templates/get_attendees.sql
@@ -5,6 +5,6 @@ select
 from thing 
 inner join thing_type on thing.type_id = thing_type.id
 where thing.type_id != 3
-and lower(thing.name) like lower('<%= query %>%')
+and lower(thing.name) like lower('%<%= query %>%')
 order by thing.name asc
 limit 50
