7641b919e1daef09e8ad1d27a06c4840cabdf6ee
William Becker <wbecker+hiveshare@gmail.com>
2014-01-25 19:53:49 +0000

    Ensure that the points are aggregated in chronological order

--- db_templates/find_event_things_by_name.sql
+++ db_templates/find_event_things_by_name.sql
@@ -1,24 +1,38 @@
 select
-  t.id as thing_id,
-  count(e.id) as event_count,
-  t.name as thing_name,
-  t.link as thing_link,
-  thing_type.name as thing_type_name,
-  min(e.start_date) as start_date,
-  max(e.end_date) as end_date,
-  ST_Extent(p.location) as area,
-  ST_AsGeoJSON(ST_MakeLine(p.location)) as points
+  matching_thing.thing_id as thing_id,
+  count(matching_thing.event_id) as event_count,
+  matching_thing.thing_name as thing_name,
+  matching_thing.thing_link as thing_link,
+  matching_thing.thing_type_name as thing_type_name,
+  min(matching_thing.start_date) as start_date,
+  max(matching_thing.end_date) as end_date,
+  ST_Extent(matching_thing.location) as area,
+  ST_AsGeoJSON(ST_MakeLine(matching_thing.location)) as points
 
-from thing t
-inner join thing_type on thing_type.id = t.type_id
-inner join event_participant ep on ep.thing_id = t.id
-inner join event e on e.id = ep.event_id
-inner join thing pt on pt.id = e.place_id
-inner join place p on p.thing_id = pt.id
+from (
+  select
+    t.id as thing_id,
+    e.id as event_id,
+    t.name as thing_name,
+    t.link as thing_link,
+    thing_type.name as thing_type_name,
+    e.start_date as start_date,
+    e.end_date as end_date,
+    p.location as location
 
-where t.name ilike $1
+  from thing t
+  inner join thing_type on thing_type.id = t.type_id
+  inner join event_participant ep on ep.thing_id = t.id
+  inner join event e on e.id = ep.event_id
+  inner join thing pt on pt.id = e.place_id
+  inner join place p on p.thing_id = pt.id
 
-group by t.id, thing_type.name
+  where t.name ilike $1
+
+  order by start_date
+) matching_thing
+
+group by thing_id, thing_name, thing_link, thing_type_name
 
 order by
   event_count desc,
