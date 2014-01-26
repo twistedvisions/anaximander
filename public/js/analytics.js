76222d3083a7018c76d53bdeb71613601f9e5c64
William Becker <wbecker+hiveshare@gmail.com>
2014-01-26 21:28:16 +0000

    Instead of having the same function repeated over and over, have a list of the different things that may be tracked.

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,5 +1,5 @@
-define([], function () {
-  return {
+define(["underscore"], function (_) {
+  var analytics = {
     ensureUniversalVariable: function () {
       if (!window.universal_variable) {
         window.universal_variable = {
@@ -19,104 +19,56 @@ define([], function () {
       data = this.ensureData(data, type);
       window.universal_variable.events.push(data);
     },
-
-    //map
-    navigation: function (data) {
-      this.sendEvent(data, "navigation");
-    },
-    infoBoxShown: function (data) {
-      this.sendEvent(data, "infoBoxShown");
-    },
-    linkClicked: function (data) {
-      this.sendEvent(data, "linkClicked");
-    },
-    mapEntrySearched: function (data) {
-      this.sendEvent(data, "mapEntrySearched");
-    },
-
-    //search
-    searchBoxCharacterTyped: function (data) {
-      this.sendEvent(data, "searchBoxCharacterTyped");
-    },
-    searchBoxStringTyped: function (data) {
-      this.sendEvent(data, "searchBoxStringTyped");
-    },
-    searchBoxCleared: function (data) {
-      this.sendEvent(data, "searchBoxCleared");
-    },
-    searchEntryLinkClicked: function (data) {
-      this.sendEvent(data, "searchLinkClicked");
-    },
-    searchEntryClicked: function (data) {
-      this.sendEvent(data, "searchLinkClicked");
-    },
-    hideSearchResults: function (data) {
-      this.sendEvent(data, "hideSearchResults");
-    },
-    searchSubmitted: function (data) {
-      this.sendEvent(data, "searchSubmitted");
-    },
-    searchPasted: function (data) {
-      this.sendEvent(data, "searchPasted");
-    },
-    searchCopied: function (data) {
-      this.sendEvent(data, "searchCopied");
-    },
-
-    //period selector
-    periodSelectorOpened: function (data) {
-      this.sendEvent(data, "periodSelectorOpened");
-    },
-    periodSelected: function (data) {
-      this.sendEvent(data, "periodSelected");
-    },
-
-    //adding events
-    optionsMenuShown: function (data) {
-      this.sendEvent(data, "optionsMenuShown");
-    },
-    optionSelected: function (data) {
-      this.sendEvent(data, "optionSelected");
-    },
-    eventAdded: function (data) {
-      this.sendEvent(data, "eventAdded");
-    },
-
-    //filters
-    showFilters: function (data) {
-      this.sendEvent(data, "showFilters");
-    },
-    filterEventsByPrimary: function (data) {
-      this.sendEvent(data, "filterEventsByPrimary");
-    },
-    filterEventsBySecondary: function (data) {
-      this.sendEvent(data, "filterEventsBySecondary");
-    },
-    filterSecondaryFilters: function (data) {
-      this.sendEvent(data, "filterSecondaryFilters");
-    },
-    toggleSecondaryFilterSelection: function (data) {
-      this.sendEvent(data, "toggleSecondaryFilterSelection");
-    },
-
-    //login
-    loginChoiceShown: function (data) {
-      this.sendEvent(data, "loginChoiceShown");
-    },
-    loginAttempted: function (data) {
-      this.sendEvent(data, "loginAttempted");
-    },
-    loginSucceeded: function (data) {
-      this.sendEvent(data, "loginSucceeded");
-    },
-    loginFailed: function (data) {
-      this.sendEvent(data, "loginFailed");
-    },
-    logout: function (data) {
-      this.sendEvent(data, "logout");
-    },
-    register: function (data) {
-      this.sendEvent(data, "register");
+    _addEvent: function (eventName) {
+      this[eventName] = _.bind(function (eventName, data) {
+        this.sendEvent(data, eventName);
+      }, this, eventName);
     }
   };
+  var events = [
+    //map
+    "navigation",
+    "infoBoxShown",
+    "linkClicked",
+    "mapEntrySearched",
+
+    //search
+    "searchBoxCharacterTyped",
+    "searchBoxStringTyped",
+    "searchBoxCleared",
+    "searchEntryLinkClicked",
+    "searchEntryClicked",
+    "hideSearchResults",
+    "searchSubmitted",
+    "searchPasted",
+    "searchCopied",
+
+    //period selector
+    "periodSelectorOpened",
+    "periodSelected",
+
+    //adding events
+    "optionsMenuShown",
+    "optionSelected",
+    "eventAdded",
+
+    //filters
+    "showFilters",
+    "filterEventsByPrimary",
+    "filterEventsBySecondary",
+    "filterSecondaryFilters",
+    "toggleSecondaryFilterSelection",
+
+    //login
+    "loginChoiceShown",
+    "loginAttempted",
+    "loginSucceeded",
+    "loginFailed",
+    "logout",
+    "register"
+  ];
+  _.each(events, function (event) {
+    analytics._addEvent(event);
+  });
+  return analytics;
 });
