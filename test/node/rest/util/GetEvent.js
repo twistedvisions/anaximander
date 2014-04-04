4cde0a484d7c12770714aae2dc5c8c81ff615657
William Becker <wbecker+hiveshare@gmail.com>
2014-04-04 07:59:55 +0100

    normalise the times returned so they are independent of timezone

--- test/node/rest/util/GetEvent.js
+++ test/node/rest/util/GetEvent.js
@@ -7,12 +7,46 @@ describe("GetEvent", function () {
   describe("handleGetEvent", function () {
     beforeEach(function () {
       this.firstRow = {
+        end_date: new Date(),
+        start_date: new Date(),
         importance_id: 20,
         type_id: 21,
         place_id: 22,
         place_name: "Paris"
       };
       this.next = function () {};
+    });
+    it("should take the local timezone ofset out of the start date, so the iso time looks right", function () {
+      var time;
+      this.firstRow.end_date = {
+        setTime: function (_time) {
+          time = _time;
+        },
+        getTime: function () {
+          return 1000000;
+        },
+        getTimezoneOffset: function () {
+          return 120;
+        }
+      };
+      getEvent.handleGetEvent([this.firstRow]);
+      time.should.equal(1000000 - 120 * 60 * 1000);
+    });
+    ("should take the local timezone ofset out of the start date, so the iso time looks right", function () {
+      var time;
+      this.firstRow.start_date = {
+        setTime: function (_time) {
+          time = _time;
+        },
+        getTime: function () {
+          return 1000000;
+        },
+        getTimezoneOffset: function () {
+          return 120;
+        }
+      };
+      getEvent.handleGetEvent([this.firstRow]);
+      time.should.equal(1000000 - 120 * 60 * 1000);
     });
     it("should make an importance object", function () {
       var event = getEvent.handleGetEvent([this.firstRow]);
@@ -44,17 +78,21 @@ describe("GetEvent", function () {
       should.not.exist(event.place_name);
     });
     it("should group results into participants", function () {
-      var event = getEvent.handleGetEvent([{}, {}]);
+      var event = getEvent.handleGetEvent([this.firstRow, this.firstRow]);
       event.participants.length.should.equal(2);
     });
     it("should group keys prefixed with participant", function () {
       var event = getEvent.handleGetEvent([
         {
+          start_date: new Date(),
+          end_date: new Date(),
           participant_id: 1,
           participant_type_id: 10,
           participant_importance_id: 100
         },
         {
+          start_date: new Date(),
+          end_date: new Date(),
           participant_id: 2,
           participant_type_id: 20,
           participant_importance_id: 200
