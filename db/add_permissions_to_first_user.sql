a3d7b1924e128ce7e5666ddd063d709a7a547797
William Becker <wbecker+hiveshare@gmail.com>
2014-05-27 08:11:27 +0100

    update permissions for first user script

--- db/add_permissions_to_first_user.sql
+++ db/add_permissions_to_first_user.sql
@@ -1,8 +1,13 @@
-insert into user_permission (user_id, permission_id)
-  values (2, 1);
-
-insert into user_permission (user_id, permission_id)
-  values (2, 2);
-
-insert into user_permission (user_id, permission_id)
-  values (2, 3);
+insert into permission_group_permission (name) values ('admin');
+insert into permission_group_permission (permission_group_id, permission_id) values
+  (2, 1),
+  (2, 2),
+  (2, 3),
+  (2, 4),
+  (2, 5),
+  (2, 6),
+  (2, 7),
+  (2, 8),
+  (2, 9)
+  ;
+update user_permission set permission_group_id = 2 where user_id = 2;
