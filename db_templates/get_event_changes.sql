5be3f3aa2bde432534b692511acf1b99512ebb66
William Becker <wbecker+hiveshare@gmail.com>
2014-06-14 14:44:11 +0100

    show old value in per event history

--- db_templates/get_event_changes.sql
+++ db_templates/get_event_changes.sql
@@ -3,6 +3,7 @@ select
   'event' as type,
   u.username,
   c.date at time zone 'utc' as date,
+  c.old as old_values,
   c.new as new_values
 from change c
 inner join registered_user u on u.id = c.user_id
